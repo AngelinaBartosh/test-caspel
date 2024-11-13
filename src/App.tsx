a1f71d611f866abee615e72b6eaa29da0ebd48f0
AngelinaBartosh <angelinabartosh2001@icloud.com>
2024-11-13 19:25:25 +0300

    init react app

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.scss';
+import TableSection from "./sections/TableSection";
+
+function App() {
+  return (
+    <div className="App">
+      <TableSection/>
+    </div>
+  );
+}
+
+export default App;
