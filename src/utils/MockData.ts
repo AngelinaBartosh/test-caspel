06b34569c6cf31019bd3498836d706775de34f60
AngelinaBartosh <angelinabartosh2001@icloud.com>
2024-11-13 19:31:28 +0300

    add test data component

---
+++ src/utils/MockData.ts
@@ -0,0 +1,14 @@
+import {DataType} from "../sections/TableSection";
+
+export const dataTest: DataType[] = [
+    {
+        key: 1,
+        name: 'Иванов Иван Иванович',
+        age: 22,
+    },
+    {
+        key: 2,
+        name: 'Смирнова Анна Сергеевна',
+        age: 27,
+    },
+];
